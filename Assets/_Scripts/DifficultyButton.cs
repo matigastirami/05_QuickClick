4452f06a4475af1bce3cf2537928c7e4ad8ecc12
Matías Ramirez <matigastirami@gmail.com>
2021-01-27 15:50:10 -0300

    Difficulty buttons functionality

---
+++ Assets/_Scripts/DifficultyButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButton : MonoBehaviour
+{
+    private Button _button;
+    
+    private GameManager gameManager;
+
+    [SerializeField]
+    [Range(1,3)]
+    private int difficulty;
+    
+    void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+        
+        _button = GetComponent<Button>();
+        
+        _button.onClick.AddListener(SetDifficulty);
+    }
+
+    void SetDifficulty()
+    {
+        gameManager.StartGame(difficulty);
+    }
+}
