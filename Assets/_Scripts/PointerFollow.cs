209efa582c3e0e82b9aa57f25b785ce11a0b741f
Matías Ramirez <matigastirami@gmail.com>
2021-01-27 23:20:14 -0300

    Trail following the pointer. Default pointer replaced by a sword pointer.

---
+++ Assets/_Scripts/PointerFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointerFollow : MonoBehaviour
+{
+
+    private Camera _camera;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = _camera.ScreenToWorldPoint(Input.mousePosition);
+
+        // It's needed to use z = 0 because if not, the camera won't be able to see the mouse trail
+        mousePos = new Vector3(mousePos.x, mousePos.y);
+
+        transform.position = mousePos;
+    }
+}
